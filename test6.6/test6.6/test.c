7bb4af3aeae9524b96fa16b68490c4d776976fce
l1lyan <124878185+l1lyan@users.noreply.github.com>
2024-06-06 22:25:01 +0800

    Add files via upload

---
+++ test6.6/test6.6/test.c
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d %d", &m, &n);
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < m; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	for (i = 0; i < n; i++)//i<n
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			printf("%d ", arr[j][i]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	int flag = 1;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < i; j++)//j<i
+//		{
+//			if (arr[i][j] != 0)
+//			{
+//				flag = 0;
+//				goto end;
+//			}
+//		}
+//	}
+//end:
+//	if (flag = 0)
+//		printf("NO\n");
+//	else
+//		printf("YES\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[50] = { 0 };
+//	int i = 0;
+//	int flag1 = 0;
+//	int flag2 = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (i > 0)
+//		{
+//			if (arr[i] > arr[i - 1])
+//				flag1 = 1;
+//			else if (arr[i] < arr[i - 1])
+//				flag2 = 1;
+//			else
+//				;
+//		}
+//	}
+//	if (flag1 + flag2 <= 1)
+//		printf("sorted\n");
+//	else
+//		printf("unsorted\n");
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
